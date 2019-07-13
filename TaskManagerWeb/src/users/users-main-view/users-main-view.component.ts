c00270a50bb03c2e21f1ee855dddb01e2f69bc9d
Vartak, Tanmay (Cognizant) <tanmay.vartak@Cognizant.com>
2019-07-13 15:44:13 +0530

    Added search and sort feature for users list

--- TaskManagerWeb/src/users/users-main-view/users-main-view.component.ts
+++ TaskManagerWeb/src/users/users-main-view/users-main-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../users.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'users-users-main-view',
@@ -8,9 +9,12 @@ import { UsersService } from '../users.service';
 })
 export class UsersMainViewComponent implements OnInit {
   userList:Array<any>
+  tempUserList:Array<any>
+  searchValue : string
   userService:UsersService
   constructor(userSer:UsersService) { 
     this.userService = userSer;
+    this.searchValue = '';
   }
 
   ngOnInit() {
@@ -21,12 +25,37 @@ export class UsersMainViewComponent implements OnInit {
     console.log("Adding user");
     console.log(user);
     this.userList.push(user);
+    this.tempUserList.push(user);
+    this.onKeyUp('');
   }
 
   refreshUsers(){
     this.userService.get().subscribe((userlist:Array<any>) => {
       this.userList = userlist;
+      this.tempUserList = userlist;
     });
+  }
+
+  onKeyUp($event){
+    this.userList = this._filterByTask('');
+  }
+
+  onSeachBtnClick(trigger){
+    if(trigger === 'firstName'){
+      this.userList = _.sortBy(this.userList, (u) => u.firstName);
+    }else if (trigger === 'lastName'){
+      this.userList = _.sortBy(this.userList, (u) => u.lastName);
+    } else if (trigger === 'id'){
+      this.userList = _.sortBy(this.userList, (u) => u.employeeID);
+    }
+  }
+
+  _filterByTask(filterValue) {
+    return this.tempUserList.filter(
+    user => user.firstName.toLowerCase().includes(this.searchValue.toLowerCase()) ||
+    user.lastName.toLowerCase().includes(this.searchValue.toLowerCase()) ||
+    user.employeeID === +this.searchValue.toLowerCase()
+  );
   }
 
 }
