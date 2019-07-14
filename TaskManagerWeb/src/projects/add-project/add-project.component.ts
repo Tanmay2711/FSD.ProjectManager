5394a7c2b0800730064351ca6315fbc3d9f9ca55
Vartak, Tanmay (Cognizant) <tanmay.vartak@Cognizant.com>
2019-07-14 13:55:37 +0530

    Added Add Project View

--- TaskManagerWeb/src/projects/add-project/add-project.component.ts
+++ TaskManagerWeb/src/projects/add-project/add-project.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FormControl } from '@angular/forms';
+import { startWith, map } from 'rxjs/operators';
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
+import { UsersService } from 'src/users/users.service';
 
 @Component({
   selector: 'app-add-project',
@@ -6,10 +11,72 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-project.component.css']
 })
 export class AddProjectComponent implements OnInit {
-
-  constructor() { }
+  myControl = new FormControl();
+  filteredUsers:Observable<Array<any>>;
+  userList:Array<any>;
+  projectInfo:any;
+  ToolTipText: string = "0";
+  constructor(private userService:UsersService) { 
+    this.projectInfo = {
+      projectID:0,
+      projectName:'',
+      startDate:'',
+      endDate:'',
+      priority:0,
+      manager:{
+        userId:0,
+        firstName:'',
+        lastName:'',
+        employeeId:0
+      }
+    };
+  }
+  
 
   ngOnInit() {
+    this.userService.get().subscribe((data : Array<any>) => {
+      this.userList = data;
+
+      this.filteredUsers = this.myControl.valueChanges
+        .pipe(
+          startWith(''),
+          map(value => typeof value === 'string' ? value : value.firstName + ' ' + value.lastName),
+          map(name => name ? this._filter(name) : this.userList.slice())
+        );
+    });
+  }
+
+  displayFn(user?: any): string | undefined {
+    return user ? user.firstName + " " + user.lastName : undefined;
+  }
+
+  //remove this for test only
+  onManagerInput($event){
+    console.log($event);
+  }
+
+  //remove this for test only
+  managerSelected($event){
+    console.log($event);
+    console.log(this.projectInfo);
+  }
+
+  addOrUpdateProjectRecord($event){
+    console.log(this.projectInfo);
+  }
+
+  onRangeInput($event){
+    this.ToolTipText = this.projectInfo.priority.toString();
+  }
+
+  onSelectClick($event){
+    console.log($event);
+  }
+
+  private _filter(name: string): any[] {
+    const filterValue = name.toLowerCase();
+
+    return this.userList.filter(option => option.firstName.toLowerCase().indexOf(filterValue) === 0 || option.lastName.toLowerCase().indexOf(filterValue) === 0);
   }
 
 }
--- TaskManagerWeb/src/projects/projects.module.ts
+++ TaskManagerWeb/src/projects/projects.module.ts
@@ -1,15 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectMainViewComponent } from './project-main-view/project-main-view.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { AddProjectComponent } from './add-project/add-project.component';
 import { ProjectsListViewComponent } from './projects-list-view/projects-list-view.component';
+import {MatAutocompleteModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatTooltipModule,
+} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatTooltipModule,
     AppRoutingModule
   ],
   declarations: [ProjectMainViewComponent,
