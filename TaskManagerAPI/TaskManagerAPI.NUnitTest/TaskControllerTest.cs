e1ec7be318386aefd02fd319033f14d2ec9c01ca
Tanmay Vartak <tanmay.vartak@ey.com>
2019-07-30 15:45:49 +0530

    Corrected test cases for Tasks controller

--- TaskManagerAPI/TaskManagerAPI.NUnitTest/TaskControllerTest.cs
+++ TaskManagerAPI/TaskManagerAPI.NUnitTest/TaskControllerTest.cs
@@ -28,7 +28,9 @@ namespace Tests
                     ParentID = 2,
                     EndDate = DateTime.Now.AddDays(3),
                     Priority = 4,
-                    StartDate = DateTime.Now
+                    StartDate = DateTime.Now,
+                    UserID = 1,
+                    ProjectID = 1
                 });
                 yield return new TestCaseData(2, new Tasks
                 {
@@ -37,7 +39,9 @@ namespace Tests
                     ParentID = 0,
                     EndDate = DateTime.Now.AddDays(6),
                     Priority = 2,
-                    StartDate = DateTime.Now
+                    StartDate = DateTime.Now,
+                    UserID = 1,
+                    ProjectID = 1
                 });
                 yield return new TestCaseData(6, new Tasks
                 {
@@ -46,7 +50,9 @@ namespace Tests
                     ParentID = 0,
                     EndDate = DateTime.Now.AddDays(6),
                     Priority = 2,
-                    StartDate = DateTime.Now
+                    StartDate = DateTime.Now,
+                    UserID = 1,
+                    ProjectID = 1
                 });
             }
         }
@@ -62,7 +68,9 @@ namespace Tests
                     ParentID = 2,
                     EndDate = DateTime.Now.AddDays(6),
                     Priority = 20,
-                    StartDate = DateTime.Now
+                    StartDate = DateTime.Now,
+                    UserID = 1,
+                    ProjectID = 1
                 });
             }
         }
@@ -75,6 +83,29 @@ namespace Tests
                        .Options;
 
             this.context = new TaskManagerContext(options);
+            var u1 = new User
+            {
+                EmployeeID = 1,
+                FirstName = "Tanmay",
+                LastName = "Vartak",
+                UserID = 1
+            };
+
+            var p1 = new Project
+            {
+                ProjectID = 1,
+                ProjectName = "EY",
+                Priority = 1,
+                ManagerID = 1,
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddDays(2),
+            };
+
+            this.context.Users.Add(u1);
+            this.context.SaveChanges();
+
+            this.context.Projects.Add(p1);
+            this.context.SaveChanges();
 
             var t1 = new Tasks
             {
@@ -83,7 +114,9 @@ namespace Tests
                 ParentID = 0,
                 EndDate = DateTime.Now.AddDays(7),
                 Priority = 1,
-                StartDate = DateTime.Now
+                StartDate = DateTime.Now,
+                UserID = 1,
+                ProjectID = 1
             };
             var t2 = new Tasks
             {
@@ -92,7 +125,9 @@ namespace Tests
                 ParentID = 1,
                 EndDate = DateTime.Now.AddDays(5),
                 Priority = 1,
-                StartDate = DateTime.Now
+                StartDate = DateTime.Now,
+                UserID = 1,
+                ProjectID = 1
             };
             var t3 = new Tasks
             {
@@ -101,7 +136,9 @@ namespace Tests
                 ParentID = 2,
                 EndDate = DateTime.Now,
                 Priority = 1,
-                StartDate = DateTime.Now.AddDays(-5)
+                StartDate = DateTime.Now.AddDays(-5),
+                UserID = 1,
+                ProjectID = 1
             };
             this.context.Tasks.AddRange(new Tasks[]
             {
@@ -110,6 +147,8 @@ namespace Tests
 
             await this.context.SaveChangesAsync();
 
+            this.context.Entry<User>(u1).State = EntityState.Detached;
+            this.context.Entry<Project>(p1).State = EntityState.Detached;
             this.context.Entry<Tasks>(t1).State = EntityState.Detached;
             this.context.Entry<Tasks>(t2).State = EntityState.Detached;
             this.context.Entry<Tasks>(t3).State = EntityState.Detached;
