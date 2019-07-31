48ab2a08aa70370cd650ab1eff2a5fe37b436455
Tanmay Vartak <tanmay.vartak@ey.com>
2019-07-31 16:27:30 +0530

    Added New Project for NBench

---
+++ TaskManagerAPI/TaskManagerAPI.NBenchTest/TestSuits.cs
@@ -0,0 +1,35 @@
+﻿using NBench;
+using TaskManagerAPI.Controllers;
+using TaskManagerAPI.Models;
+
+namespace TaskManager.PerformanceTest
+{
+    public class TestSuits : PerformanceTest<TestSuits>
+    {
+        private TaskManagerContext context;
+
+        [PerfBenchmark(RunMode = RunMode.Iterations, TestMode = TestMode.Measurement)]
+        [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
+        public void GetTaskMemory_Test()
+        {
+            var taskController = new TasksController(context);
+            var response = taskController.GetTasks();
+        }
+
+        [PerfBenchmark(RunMode = RunMode.Iterations, TestMode = TestMode.Measurement)]
+        [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
+        public void GetParentTaskMemory_Test()
+        {
+            var taskController = new TasksController(context);
+            var response = taskController.GetTasks();
+        }
+
+        [PerfBenchmark(RunMode = RunMode.Iterations, TestMode = TestMode.Measurement)]
+        [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
+        public void GetTaskByIdMemory_Test()
+        {
+            var taskController = new TasksController(context);
+            var response = taskController.GetTasks(1);
+        }
+    }
+}
