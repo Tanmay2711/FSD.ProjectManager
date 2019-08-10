227d3b4a3a287d0dd4200c85d8da9ad1c65ec9ae
Vartak, Tanmay (Cognizant) <tanmay.vartak@Cognizant.com>
2019-08-10 18:24:25 +0530

    Added jasmin test for user service

--- TaskManagerWeb/src/users/users.service.spec.ts
+++ TaskManagerWeb/src/users/users.service.spec.ts
@@ -5,14 +5,14 @@ import { UsersService } from './users.service';
 import { of } from 'rxjs';
 
 describe('Service: UsersService Tests', () => {
-  let httpClientSpy: { get: jasmine.Spy };
+  let httpClientSpy: { get: jasmine.Spy, post:jasmine.Spy,put:jasmine.Spy, delete:jasmine.Spy };
   let userService: UsersService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [UsersService]
     });
-    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get','post','put','delete']);
     userService = new UsersService(<any> httpClientSpy);
   });
 
@@ -58,4 +58,54 @@ describe('Service: UsersService Tests', () => {
     expect(httpClientSpy.get.calls.count()).toBe(1, 'get method on http client stubb should get called once.');
   });
 
+  it('should create expected user and HttpClient post method should get called once',()=>{
+    const user = {
+      userID: 1, 
+      firstName: 'Tanmay',
+      lastName:'Vartak',
+      employeeID:1 
+    };
+
+    httpClientSpy.post.and.returnValue(of(user));
+    userService.add(user).subscribe(
+      userData => expect(userData).toEqual(user, 'Users service should return exact user which is stubbed'),
+      fail
+    );
+
+    expect(httpClientSpy.post.calls.count()).toBe(1, 'post method on http client stubb should get called once.');
+  });
+
+  it('should update expected user and HttpClient put method should get called once',()=>{
+    const user = {
+      userID: 1, 
+      firstName: 'Tanmay',
+      lastName:'Vartak',
+      employeeID:1 
+    };
+
+    httpClientSpy.put.and.returnValue(of(user));
+    userService.update(user).subscribe(
+      userData => expect(userData).toEqual(user, 'Users service should return exact user which is stubbed'),
+      fail
+    );
+
+    expect(httpClientSpy.put.calls.count()).toBe(1, 'put method on http client stubb should get called once.');
+  });
+
+  it('should delete expected user and HttpClient delete method should get called once',()=>{
+    const user = {
+      userID: 1, 
+      firstName: 'Tanmay',
+      lastName:'Vartak',
+      employeeID:1 
+    };
+
+    httpClientSpy.delete.and.returnValue(of(user));
+    userService.remove(user).subscribe(
+      userData => expect(userData).toEqual(user, 'Users service should return exact user which is stubbed'),
+      fail
+    );
+
+    expect(httpClientSpy.delete.calls.count()).toBe(1, 'put method on http client stubb should get called once.');
+  });
 });
